652b121b401fb198a5a0892871002a809c925290
aws1313 <69417093+aws1313@users.noreply.github.com>
2024-07-06 11:04:45 +0200

    initial ideas

---
+++ notenrechner/android/app/src/main/kotlin/com/example/notenrechner/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.notenrechner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ umfragen/android/app/src/main/kotlin/com/example/umfragen/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.umfragen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
